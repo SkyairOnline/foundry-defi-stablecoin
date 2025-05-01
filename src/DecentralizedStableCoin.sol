c9252da7ea6161250926b0a971954136b32e87a8
skyaironline <aldo@batamon.asia>
2025-05-01 12:41:03 +0700

    Deploy script DSCEngine

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -14,7 +14,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system
  * 
  */
-contract DecentralizedStableCoin is ERC20Burnable, Ownable(msg.sender) {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__MustBeMoreThanZero();
     error DecentralizedStableCoin__BurnAmoundExceedsBalance();
     error DecentralizedStableCoin__NotZeroAddress();
